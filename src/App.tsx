a00f5326950399ab8f4868950138d2dd917d84a3
HadalyVillasclaras <hadalyvf@gmail.com>
2023-03-01 09:07:30 +0100

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default App
