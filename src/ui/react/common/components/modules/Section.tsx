2368bf3e2acd1bf664b22bf95405fc70886ad14c
HadalyVillasclaras <hadalyvf@gmail.com>
2023-03-01 11:58:27 +0100

    feature: section component

---
+++ src/ui/react/common/components/modules/Section.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react'
+import { Box } from '@chakra-ui/react'
+
+
+interface Props {
+    children: ReactElement | ReactElement[]
+}
+
+export const Section = ({ children }: Props) => {
+  return (
+    <Box as="section" backgroundColor='white' p='2rem' borderRadius='1rem' mb='2rem'>
+        {children}
+    </Box>
+  )
+}
