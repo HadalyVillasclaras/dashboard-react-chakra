a845c42478123d9ea0fbcbc502b3679783ce10e5
HadalyVillasclaras <hadalyvf@gmail.com>
2023-03-15 13:48:04 +0100

    feat: is loading spinner

--- src/ui/react/modules/Books/components/BooksContainer.tsx
+++ src/ui/react/modules/Books/components/BooksContainer.tsx
@@ -1,21 +1,23 @@
 
 import { AddIcon } from "@chakra-ui/icons";
-import { Button, Divider, Flex } from "@chakra-ui/react";
+import { Button, Divider, Flex, Spinner } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { Section } from "../../../common/components/Section";
 import { BooksList } from "./BooksList";
-import booksLib from "../../../../../modules/core/books.json";
 import { useEffect, useState } from "react";
 import { Book } from "../../../../../modules/books/models/book.model";
 import { getBooks as getBooksService } from "../../../../../modules/books/services/getBooks";
 
 export const BooksContainer = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+
 
   async function getBooks() {
     await getBooksService()
       .then((res) => {
         setBooks(res);
+        setIsLoading(false)
       })
       .catch((err) => {
         console.log(err);
@@ -34,6 +36,13 @@ export const BooksContainer = () => {
       <h4>Filters:</h4>
       <Divider m="1rem 0"/>
     </Section>
+    {isLoading ? (
+      <Section>
+      <div style={{ textAlign: "center" }}>
+        <Spinner />
+      </div>
+    </Section>
+    ) : (
     <Section>
       <Flex justifyContent="space-between" mb="1rem">
         <h4>List:</h4>
@@ -48,6 +57,7 @@ export const BooksContainer = () => {
         books={books}
       />
     </Section>
+    )}
     <Divider m="2rem 0" />
 
     </>
