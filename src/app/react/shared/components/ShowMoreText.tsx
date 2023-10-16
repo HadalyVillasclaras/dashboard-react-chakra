7005e0dc03cd7f6f3839309fdd58cb3874354eda
Hadaly <hadalyvf@gmail.com>
2023-10-16 18:40:04 +0200

    feat: show more text

---
+++ src/app/react/shared/components/ShowMoreText.tsx
@@ -0,0 +1,52 @@
+import { useState, useRef, useEffect } from 'react';
+import { Button } from '../design-system/atoms';
+import styles from './ShowMoreText.module.scss';
+
+interface Props {
+  text: string;
+  textLength?: number;
+}
+
+export const ShowMoreText = ({ text, textLength = 300 }: Props) => {
+  const [showMoreText, setShowMoreText] = useState(false);
+  const [contentHeight, setContentHeight] = useState(null);
+  const contentRef = useRef(null);
+
+  const ending = text.length > textLength ? "..." : "";
+  const slicedText = text.slice(0, textLength) + ending;
+  const displayedText = showMoreText ? text : slicedText;
+
+  console.log(contentHeight);
+
+  const toggleShowMore = () => {
+    setShowMoreText(!showMoreText);
+  };
+
+  useEffect(() => {
+    if (contentRef?.current) {
+      setContentHeight(contentRef?.current?.scrollHeight);
+    }
+  }, [showMoreText]);
+
+
+  return (
+    <div className={styles.showMoreTextContainer}>
+      <div 
+        className={styles.textContainer} 
+        style={{ height: contentHeight}}
+      >
+        <p ref={contentRef} className={showMoreText ? styles.expandedText : styles.collapsedText}>
+          {displayedText}
+        </p>
+      </div>
+      {text.length > textLength &&
+        <Button 
+          variant='underline' 
+          color='cream' 
+          text={showMoreText ? 'Collapse ^' : 'Know more about it'} 
+          onClick={toggleShowMore}
+        />
+      }
+    </div>
+  );
+}
